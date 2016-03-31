1723685961bf2de03684c6bb0c13df07e7a17f33
Niclas Maabari Skovbjerg <niclas87@gmail.com>
2016-03-31 20:28:36 +0200

    Update complete.R

--- complete.R
+++ complete.R
@@ -1,3 +1,8 @@
+setwd() <- "C:/Users/nskovbjerg/Desktop/Coursera/rprog-data-specdata/specdata" ## NoTE: INSERT THE WORKING DIRECTORY PATH HERE
+
+## START FUNCTION ##
+
+directory <- getwd() ##ASSIGN WORWING DIRECTORY PATH TO DIRECTORY
 complete <- function(directory, id = 1:332) {
         filelist <- list.files(directory, full.names = TRUE)
         dataframe <- data.frame()
@@ -12,7 +17,12 @@ complete <- function(directory, id = 1:332) {
         dataframe
 }
 
-complete(getwd(), 1)                    #CHECK: OK!
+## END FUNCTION ##
+
+
+## ************************** C H E C K ************************** ##
+
+complete(getwd(), 1)                    #result 117. CHECK: OK!
 complete(getwd(), c(2, 4, 8, 10, 12))   #CHECK: OK!
 complete(getwd(), 30:25)                #CHECK: OK!
 complete(getwd(), 3)                    #CHECK: OK!
